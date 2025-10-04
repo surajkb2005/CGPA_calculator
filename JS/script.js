56d2d5f8669c2c724654dd38473f7d817c8e6c52
Suraj <skbanavalikar2105@gmail.com>
2025-10-04 17:15:56 +0530

    the calculate CGPA is working properly

--- JS/script.js
+++ JS/script.js
@@ -79,7 +79,6 @@ function predict(){
     }else{
         displayPredictionVar.innerHTML += "<BR>7. Sorry, Passing not possible";
     }
-    //written soon
 }
 
 
@@ -87,6 +86,78 @@ function predict(){
 
 
 
+//for calculate cgpa
+tdisp = document.getElementById('tabDisp');
+var studDetails = [];
+var idx=1;
+displayDetails();
+function displayDetails(){
+    setTimeout(()=>{
+        var tcon = "<tr>";
+        tcon += "<th>Sl No. </th> <th>Subject</th> <th>Marks</th> <th>Credit</th>";
+        tcon += "</tr>";
+        for(var i=0;i<studDetails.length;i++){
+            tcon += "<tr>";
+            tcon += "<td>"+(i+1)+"</td>";
+            tcon += "<td>"+studDetails[i].course+"</td>";
+            tcon += "<td>"+studDetails[i].marks+"</td>";
+            tcon += "<td>"+studDetails[i].credit+"</td>";
+            tcon += "<td>"+`<button class="remRow" onclick='removeRow(${studDetails[i].index})'>remove</button>`+"</td>";
+            tcon += "</tr>";
+        }
+        tdisp.innerHTML = tcon;
+    },200);
+}
+function additems(){
+    var dcourse = document.getElementById("courseid").value;
+    var dmarks = document.getElementById("marksid").value;
+    var dcredits = document.getElementById("creditsid").value;
 
-// req = 100 - ((realmarks-30)*2);
-//req = 60 + ((realmarks-30)%10)*2;
+    if(dcourse && dmarks && dcredits){
+        studDetails.push({index:idx,course:dcourse,marks:dmarks,credit:dcredits});
+        idx=idx+1; //increment unique id each time
+        displayDetails();
+        clearInput();
+    }
+}
+function clearInput(){
+    document.getElementById("courseid").value="";
+    document.getElementById("marksid").value="";
+    document.getElementById("creditsid").value="";
+}
+function removeRow(id){
+    var fil = studDetails.filter((a,i)=>{
+        if(id==a.index){
+            studDetails.splice(i,1);
+        }
+    });
+    displayDetails();
+}
+function calculatecgpa(){
+    var tot=0;
+    var totcredits=0;
+    tdisp.innerHTML +="<br>";
+
+    for(var i=0;i<studDetails.length;i++){
+        //console.log("stud marks = "+studDetails[i].marks+"stud credit"+studDetails[i].credit+"<br>"); //for testing
+        if(studDetails[i].marks<0 || 100<studDetails[i].marks){
+            tdisp.innerHTML += "Marks of "+studDetails[i].course+" is greater than 100.";
+            tdisp.innerHTML += "<br> Please Enter the marks between 1 to 100";
+            return;
+        }
+        if(studDetails[i].credit<0){
+            tdisp.innerHTML += "Credits of "+studDetails[i].course+" is less than 0.<br> Please Enter Positive Value";
+            return;
+        }
+
+        var gradePoint = Math.floor((studDetails[i].marks)/10)+1;
+        if(gradePoint==11) gradePoint=10;
+        var creditPoint = parseInt(studDetails[i].credit);
+
+        totcredits += creditPoint;
+        tot += (gradePoint * creditPoint);
+    }
+
+    console.log("tot = "+tot+" totcredit"+totcredits+"<br>"); //for testing
+    tdisp.innerHTML += "Your Expected CGPA is "+(tot/totcredits);
+}
